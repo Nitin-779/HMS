c0bba261eb42a1bb90494871c837e4a7a73626a4
Ajay Aggarwal <aggarwalnaitik993@gmail.com>
2025-02-13 18:43:25 +0530

    Remove all files

--- Day1/sisd.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int numbers[]={1,2,3,4,5};
-    for (int i=0;i<5;i++){
-        cout<<"Number: "<<numbers[i]<<endl;
-    }
-    return 0;
-}
